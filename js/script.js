cf40f37c85029455fc123b84278191923bd7969d
marcosrobvel <marcosraranjuez@gmail.com>
2025-01-23 17:35:53 +0100

    crearFuncionEnviarDatosALaAPIConFetch

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ window.addEventListener('load', () => {
 })
 
 // funcion cuenta       | cantidad px scrolleados |   alto total del body   |   alto total ventana buscador |
-function update(){
+function update() {
     progress.style.width = `${((window.scrollY) / (document.body.scrollHeight - window.innerHeight) * 100)}%`;
     requestAnimationFrame(update);
 }
@@ -36,7 +36,7 @@ function update(){
 const returnTop = document.querySelector(".returnTop");
 
 window.addEventListener("scroll", () => {
-    if (window.pageYOffset > 100){
+    if (window.pageYOffset > 100) {
         returnTop.classList.add("active");
         navMenu.classList.remove("visible");
     } else {
@@ -47,114 +47,168 @@ window.addEventListener("scroll", () => {
 // ---------------------------------------- Fin funcionalidad boton "Return to top" ----------------------------------------
 
 
-// ---------------------------------------- Validacion Inputs NOMBRE y EMAIL ----------------------------------------
+// ---------------------------------------- Validacion Inputs NOMBRE, EMAIL y CHECK ----------------------------------------
 
-    var nombre = document.getElementById('inputName');
-    var labelName = document.getElementById('labelName');
-    var correo = document.getElementById('inputEmail');
-    var labelEmail = document.getElementById('labelEmail');
-    var consent = document.getElementById('inputCheck');
-    var labelConsent = document.getElementById('labelCheck');
-    var formulario = document.getElementById('formulario');
+var nombre = document.getElementById('inputName');
+var labelName = document.getElementById('labelName');
+var correo = document.getElementById('inputEmail');
+var labelEmail = document.getElementById('labelEmail');
+var consent = document.getElementById('inputCheck');
+var labelConsent = document.getElementById('labelCheck');
+var formulario = document.getElementById('formulario');
 
-    var mensajeError = '';
-    var errorNombre = false;
-    var errorCorreo = false;
-    var errorCheck = false;
+var mensajeError = '';
+var errorNombre = false;
+var errorCorreo1 = false;
+var errorCorreo2 = false;
+var errorCheck = false;
+
+var MensajeErrorNombre = 'El campo de nombre es obligatorio.\n';
+var MensajeErrorCorreo1 = 'El campo de correo es obligatorio.\n';
+var MensajeErrorCorreo2 = 'El correo no tiene un formato válido.\n';
+var MensajeErrorCheck = 'Debes aceptar el consentimiento para procesar tus datos.\n';
+
+function recogerDatos() {
+    return {
+        'nombre': nombre.value,
+        'correo': correo.value,
+        'consent': consent.checked
+    };
+}
+
 
 function validacionFormulario() {
-    
 
-    
 
-    function redBorder(element){
+
+
+    function redBorder(element) {
         element.style.borderColor = 'red';
     }
 
-    function redText(element){
+    function redText(element) {
         element.style.color = 'red';
     }
 
-    function correctBorder(element){
+    function correctBorder(element) {
         element.style.borderColor = '#A5A5A5'
 
     }
 
-    function correctText(element){
+    function correctText(element) {
         element.style.color = '#08A6E4'
 
     }
 
 
-    
 
-    
+
+
     if (nombre.value === '') {
         errorNombre = true;
-        mensajeError += 'El campo de nombre es obligatorio.\n';
+        MensajeErrorNombre;
         redBorder(nombre)
         redText(labelName)
     } else {
         errorNombre = false;
+        document.getElementById('warnings').innerText = '';
         correctBorder(nombre)
         correctText(labelName)
     }
 
-    
+
     var correoRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     if (correo.value === '') {
-        errorCorreo = true;
-        mensajeError += 'El campo de correo es obligatorio.\n';
+        errorCorreo1 = true;
+        MensajeErrorCorreo1;
         redBorder(correo)
         redText(labelEmail)
     } else if (!correoRegex.test(correo.value)) {
-        errorCorreo = true;
-        mensajeError += 'El correo no tiene un formato válido.\n';
+        errorCorreo2 = true;
+        MensajeErrorCorreo2;
         redBorder(correo)
         redText(labelEmail)
     } else {
-        errorCorreo = false;
+        errorCorreo1 = false;
+        errorCorreo2 = false;
+        document.getElementById('warnings').innerText = '';
         correctBorder(correo)
         correctText(labelEmail)
     }
 
-    
+
     if (!consent.checked) {
         errorCheck = true;
-        mensajeError += 'Debes aceptar el consentimiento para procesar tus datos.\n';
+        MensajeErrorCheck;
         redText(labelConsent)
     } else {
         errorCheck = false;
+        document.getElementById('warnings').innerText = '';
         correctText(labelConsent)
     }
 
 
-    if (errorNombre || errorCorreo || errorCheck) {
-        console.log('hay errores')
-        document.getElementById('warnings').innerText = mensajeError;
-    } else {
-        console.log('NO hay errores')
+    if (errorNombre) {
+        if (!document.getElementById('warnings').innerText.includes(MensajeErrorNombre)) {
+            document.getElementById('warnings').innerText += MensajeErrorNombre;
+        }
+    }
+    if (errorCorreo1) {
+        if (!document.getElementById('warnings').innerText.includes(MensajeErrorCorreo1)) {
+            document.getElementById('warnings').innerText += MensajeErrorCorreo1;
+        }
+    }
+    if (errorCorreo2) {
+        if(!document.getElementById('warnings').innerText.includes(MensajeErrorCorreo2)){
+            document.getElementById('warnings').innerText += MensajeErrorCorreo2;
+        }
+    }
+    if (errorCheck) {
+        if(!document.getElementById('warnings').innerText.includes(MensajeErrorCheck)){
+            document.getElementById('warnings').innerText += MensajeErrorCheck;
+        }
+    }
+
+    else {
         correctBorder(nombre)
         correctText(labelName)
         correctBorder(correo)
         correctText(labelEmail)
         correctText(labelConsent)
-        console.log({nombre: nombre.value, correo: correo.value, consent: consent.value})
         alert('Formulario enviado correctamente');
     }
+
+    return !errorNombre && !errorCorreo1 && !errorCorreo2 && !errorCheck;
 };
 
 
-function submitForm(event){
+// ---------------------------------------- Fin validacion Inputs NOMBRE y EMAIL ----------------------------------------
 
-    console.log('hola')
+// ---------------------------------------- Enviar formulario a API ----------------------------------------
 
+
+function enviarDatos(info) {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify(info),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+}
+
+
+function submitForm(event) {
     event.preventDefault()
-
-    validacionFormulario()
-
+    const isValid = validacionFormulario();
+    if (isValid){
+        enviarDatos(recogerDatos());
+    }
 }
 
 formulario.addEventListener('submit', submitForm)
 
-// ---------------------------------------- Fin validacion Inputs NOMBRE y EMAIL ----------------------------------------
+
+// ---------------------------------------- Fin enviar formulario a API ----------------------------------------
